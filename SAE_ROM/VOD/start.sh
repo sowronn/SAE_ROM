8eddcf17a0722607b00883ea04d2c256df7bf570
sowronn <101816645+sowronn@users.noreply.github.com>
2024-06-21 00:51:50 +0200

    Update start.sh

--- SAE_ROM/VOD/start.sh
+++ SAE_ROM/VOD/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 while true; do
-  # Start VLC with HTTP streaming
-  cvlc -vvv /home/vlcuser/test.mp4 --sout "#http{mux=ts,dst=:8080}" || true
+  # Démarrer VLC en mode VOD
+  cvlc -vvv /home/vlcuser/test.mp4 --sout '#standard{access=http,mux=ts,dst=:8080}'
   echo "VLC a terminé. Relancer dans 5 secondes..."
   sleep 5
 done
